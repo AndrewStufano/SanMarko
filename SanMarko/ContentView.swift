c651c71d351ba01f364c4aab632a0104df523ee3
AndrewStufano <stufanoandrew@gmail.com>
2024-07-16 17:58:11 -0400

    Initial Commit

---
+++ SanMarko/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SanMarko
+//
+//  Created by Andrew Stufano on 7/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("San Marko")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SanMarko/SanMarkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SanMarkoApp.swift
+//  SanMarko
+//
+//  Created by Andrew Stufano on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SanMarkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
